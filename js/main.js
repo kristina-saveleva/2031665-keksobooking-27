c0397f9b6d960a384ec93357efae685e1bb54c04
Kristina <tina.-.kristina@mail.ru>
2022-10-28 20:23:22 +0300

    fixed mistakes

--- js/main.js
+++ js/main.js
@@ -1,18 +1,18 @@
 function rundomNumber(min, max) {
-    if (min >= max || min<=0) {
-        return NaN;
-    } else {
-        return Math.floor(Math.random() * (max - min + 1)) + min;
-    }
+  if (min >= max || min <= 0) {
+    return NaN;
+  } else {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
 }
 console.log(rundomNumber(4, 22));
 
 function rundomNumbersDot(min, max, dot) {
-    if (min >= max || min<=0) {
-        return NaN;
-    } else {
-        let number = Math.random() * (max - min + 1) + min;
-        return number.toFixed(dot);
-    }
+  if (min >= max || min <= 0) {
+    return NaN;
+  } else {
+    let number = Math.random() * (max - min + 1) + min;
+    return number.toFixed(dot);
+  }
 }
 console.log(rundomNumbersDot(2, 400, 4));
