6d82a9c2673f9e1f918d1f5c289c9e64482f001b
Kristina <tina.-.kristina@mail.ru>
2022-11-17 22:47:35 +0300

    changed let const

--- js/createCard.js
+++ js/createCard.js
@@ -1,7 +1,7 @@
 import { generateArray } from './data.js';
 
-let makeElement = function (tagName, className, text) {
-  let element = document.createElement(tagName);
+const makeElement = function (tagName, className, text) {
+  const element = document.createElement(tagName);
   element.classList.add(className);
   if (text) {
     element.textContent = text;
@@ -9,91 +9,90 @@ let makeElement = function (tagName, className, text) {
   return element;
 };
 
-let elementsArray = generateArray();
-export let createCards = function () {
-  let cards = [];
-  for (let i = 0; i < elementsArray.length; i++) {
-    cards[i] = createCard(elementsArray[i]);
-  }
-  return cards
-}
-
-let createCard = function (product) {
-  let listItem = makeElement('article', 'popup');
-  let title = makeElement('h3', 'popup__title', product.offer.title);
+const createCard = function (product) {
+  const listItem = makeElement('article', 'popup');
+  const title = makeElement('h3', 'popup__title', product.offer.title);
   listItem.appendChild(title);
-  let address = makeElement('p', 'popup__text--address', product.offer.address);
+  const address = makeElement('p', 'popup__text--address', product.offer.address);
   listItem.appendChild(address);
-  let price = makeElement('p', 'popup__text--price', product.offer.price + ' ₽/ночь');
+  const price = makeElement('p', 'popup__text--price', product.offer.price + ' ₽/ночь');
   listItem.appendChild(price);
   function whichType() {
-    let typeOfHouse = product.offer.type;
+    const typeOfHouse = product.offer.type;
     if (typeOfHouse === 'flat') {
-      return 'Квартира'
+      return 'Квартира';
     } else if (typeOfHouse === 'bungalow') {
-      return 'Бунгало'
+      return 'Бунгало';
     } else if (typeOfHouse === 'house') {
-      return 'Дом'
+      return 'Дом';
     } else if (typeOfHouse === 'palace') {
-      return 'Дворец'
+      return 'Дворец';
     } else if (typeOfHouse === 'hotelra') {
-      return 'Отель'
+      return 'Отель';
     }
   }
-  let type = makeElement('h4', 'popup__type', whichType());
+  const type = makeElement('h4', 'popup__type', whichType());
   listItem.appendChild(type);
-  let sizeRoom = makeElement('p', 'popup__text--capacity', product.offer.rooms + ' комнаты для ' + product.offer.guests + ' гостей');
+  const sizeRoom = makeElement('p', 'popup__text--capacity', `${product.offer.rooms} комнаты для ${product.offer.guests} гостей`);
   listItem.appendChild(sizeRoom);
-  let checkTime = makeElement('p', 'popup__text--time', 'Заезд после ' + product.offer.checkin + ', выезд до ' + product.offer.checkout);
+  const checkTime = makeElement('p', 'popup__text--time', `Заезд после ${product.offer.checkin} выезд до ${product.offer.checkout}`);
   listItem.appendChild(checkTime);
-  let features = makeElement('ul', 'popup__features');
+  const features = makeElement('ul', 'popup__features');
   listItem.appendChild(features);
-  let featureArray = product.offer.features;
+  const featureArray = product.offer.features;
   for (let i = 0; i < featureArray.length; i++) {
-    let featuresItem = makeElement('li', 'popup__feature', featureArray[i]);
-    let featuresItemOne = featureArray[i];
+    const featuresItem = makeElement('li', 'popup__feature', featureArray[i]);
+    const featuresItemOne = featureArray[i];
     if (featuresItemOne === 'wifi') {
-      featuresItem.classList.add('popup__feature--wifi')
+      featuresItem.classList.add('popup__feature--wifi');
     } else if (featuresItemOne === 'dishwasher') {
-      featuresItem.classList.add('popup__feature--dishwasher')
+      featuresItem.classList.add('popup__feature--dishwasher');
     } else if (featuresItemOne === 'parking') {
-      featuresItem.classList.add('popup__feature--parking')
+      featuresItem.classList.add('popup__feature--parking');
     } else if (featuresItemOne === 'washer') {
-      featuresItem.classList.add('popup__feature--washer')
+      featuresItem.classList.add('popup__feature--washer');
     } else if (featuresItemOne === 'elevator') {
-      featuresItem.classList.add('popup__feature--elevator')
+      featuresItem.classList.add('popup__feature--elevator');
     } else if (featuresItemOne === 'conditioner') {
-      featuresItem.classList.add('popup__feature--conditioner')
+      featuresItem.classList.add('popup__feature--conditioner');
     }
     features.appendChild(featuresItem);
   }
-  let description = makeElement('p', 'popup__description', product.offer.description);
+  const description = makeElement('p', 'popup__description', product.offer.description);
   listItem.appendChild(description);
-  let photoList = makeElement('div', 'popup__photos');
+  const photoList = makeElement('div', 'popup__photos');
   listItem.appendChild(photoList);
-  let photoArray = product.offer.photo;
+  const photoArray = product.offer.photo;
   for (let i = 0; i < photoArray.length; i++) {
-    let photos = makeElement('img', 'popup__photos', photoArray[i]);
+    const photos = makeElement('img', 'popup__photos', photoArray[i]);
     photoList.appendChild(photos);
   }
-  let picture = makeElement('img', 'popup__avatar');
+  const picture = makeElement('img', 'popup__avatar');
   picture.src = product.author.avatar;
   picture.alt = 'Фото жилья';
   listItem.appendChild(picture);
   return listItem;
 };
-let firstCard = createCards();
-let getCard = function () {
-  firstCard;
-  for (let i = 0; i < firstCard.length; i++) {
-    firstCard = firstCard[i]
+
+const elementsArray = generateArray();
+export const createCards = function () {
+  const cards = [];
+  for (let i = 0; i < elementsArray.length; i++) {
+    cards[i] = createCard(elementsArray[i]);
   }
-  return firstCard
-}
+  return cards;
+};
+let firstCard = createCards();
+const getCard = function () {
+  for (let i = 0; i < firstCard.length; i++) {
+    firstCard = firstCard[i];
+  }
+  return firstCard;
+};
 firstCard = getCard();
-let templateProfile = document.querySelector('#card').content;
-let article = templateProfile.querySelector('.popup');
+const templateProfile = document.querySelector('#card').content;
+const article = templateProfile.querySelector('.popup');
 let task = article.cloneNode(true);
 task = firstCard;
-let containerMessages = document.querySelector('.map__canvas');
+const containerMessages = document.querySelector('.map__canvas');
 containerMessages.appendChild(task);
