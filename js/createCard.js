c90efa93542b0183634d584bd5f6eaa4db5a99da
Kristina <tina.-.kristina@mail.ru>
2022-11-19 19:31:39 +0300

    fixed var let

--- js/createCard.js
+++ js/createCard.js
@@ -1,5 +1,3 @@
-import { generateArray } from './data.js';
-
 const templateProfile = document.querySelector('#card').content;
 const article = templateProfile.querySelector('.popup');
 
@@ -11,7 +9,7 @@ const typeOfHouse = {
   hotelra: 'Отель'
 };
 
-  const createCard = function (product) {
+const createCard = function (product) {
   const task = article.cloneNode(true);
   const containerMessages = document.querySelector('.map__canvas');
   task.querySelector('.popup__title').textContent = product.offer.title;
@@ -22,16 +20,16 @@ const typeOfHouse = {
   task.querySelector('.popup__text--time').textContent = `Заезд после ${product.offer.checkin} выезд до ${product.offer.checkout}`;
   if (product.offer.features.length !== 0) {
     task.querySelector('.popup__features').innerHTML = '';
-      var feature;
-    for (var i = 0; i < product.offer.features.length; i++) {
+      let feature;
+  for (let i = 0; i < product.offer.features.length; i++) {
       feature = document.createElement('li');
       feature.classList.add('popup__feature');
       feature.classList.add(`popup__feature--${product.offer.features[i]}`);
       task.querySelector('.popup__features').appendChild(feature);
-    }
+  }
   } else {
     task.querySelector('.popup__features').remove();
-}
+  }
   const photoTemplate = task.querySelector('.popup__photo');
   task.querySelector('.popup__photos').innerHTML = '';
   let photography;
