686b06ffd24a9a2a15f601c1d8ffb4648922b53a
Kristina <tina.-.kristina@mail.ru>
2022-11-28 11:49:58 +0300

    added reset for filters

--- js/form-reset.js
+++ js/form-reset.js
@@ -5,10 +5,12 @@ import { resetSlider } from './slider.js';
 
 const buttonReset = document.querySelector('.ad-form__reset');
 const adForm = document.querySelector('.ad-form');
+const mapFormElement = document.querySelector('.map__filters');
 
 export const resetFormForAllElements = function(){
   adForm.reset();
   resetPristine();
+  mapFormElement.reset();
   resetMarkPosition();
   closePopup();
   createMarker();
